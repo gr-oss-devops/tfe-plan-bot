45359edae70dfaa2d8a2f04a760ef1c4c0c8b0fd
Ljubo Nikolić <ljubonikolic@gmail.com>
2024-05-30 18:34:49 +0200

    remove comments

--- cmd/server_test.go
+++ cmd/server_test.go
@@ -22,33 +22,3 @@ func TestServerCmd(t *testing.T) {
 		t.Errorf("Expected error '%s', got '%s'", expectedError, err.Error())
 	}
 }
-
-// TODO: fix/add tests
-// func TestServerCmd_EmptyConfig(t *testing.T) {
-// 	cmd := &cobra.Command{
-// 		Use: "tfe-plan-bot",
-// 	}
-// 	serverCmdConfig.Path = "empty.yml"
-
-// 	err := serverCmd(cmd, []string{})
-// 	if err == nil {
-// 		t.Errorf("Expected error due to empty config file, got nil")
-// 	}
-
-// 	expectedError := "failed to read server config: empty config file: empty.yml"
-// 	if err.Error() != expectedError {
-// 		t.Errorf("Expected error '%s', got '%s'", expectedError, err.Error())
-// 	}
-// }
-
-// func TestServerCmd_ValidConfig(t *testing.T) {
-// 	cmd := &cobra.Command{
-// 		Use: "tfe-plan-bot",
-// 	}
-// 	serverCmdConfig.Path = "tfe-plan.yml"
-
-// 	err := serverCmd(cmd, []string{})
-// 	if err != nil {
-// 		t.Errorf("Expected no error, got '%s'", err.Error())
-// 	}
-// }
